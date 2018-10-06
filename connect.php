658c1483416338b4c85026fb3184bb8dbc918022
Amine Abri <amineabri@milexa.com>
2018-10-06 15:13:58 +0100

    Fixes

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "127.0.0.1";
 $username = "homestead";
-$password = "password";
+$password = "secret";
 
 //try {
 //    $conn = new PDO("mysql:host=$servername", $username, $password);
